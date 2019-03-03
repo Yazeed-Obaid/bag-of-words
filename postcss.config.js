6d3895763d9433bb2e371d8e35de24460267d014
Yazeed Obaid <yazeeds.obaid@gmail.com>
2019-03-03 18:35:33 +0200

    Fix in minification using purgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -3,15 +3,31 @@ const tailwindJS = join(__dirname, 'tailwind.js')
 const purgecss = require('@fullhuman/postcss-purgecss')
 const cssnano = require('cssnano')
 
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:/]+/g) || []
+  }
+}
+
 module.exports = {
   plugins: [
     require('tailwindcss')(tailwindJS),
-    cssnano({
-      preset: 'default',
-    }),
     purgecss({
-      content: ['./pages/**/*.vue', './layouts/**/*.vue', './components/**/*.vue'],
-      whitelist: ['html', 'body']
+      content: [
+        './pages/**/*.vue',
+        './layouts/**/*.vue',
+        './components/**/*.vue'
+      ],
+      whitelist: ['html', 'body'],
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ['vue']
+        }
+      ]
+    }),
+    cssnano({
+      preset: 'default'
     }),
     require('autoprefixer')
   ]
